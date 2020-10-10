38344d211823466d399dd10d118426e7810568a9
ljg <740390227@qq.com>
2020-10-10 13:49:19 +0800

    合并

--- pages/latelyLtnList/latelyLtnList.js
+++ pages/latelyLtnList/latelyLtnList.js
@@ -13,6 +13,21 @@ Page({
     // 这里还要判断一下点击的歌曲是 否是正在播放的歌曲
     const id = wx.getStorageSync('songInfo') ? wx.getStorageSync('songInfo').id : null
     const sameFlag = id === e.currentTarget.dataset.id
+    
+    const songInfo = {
+      name: e.currentTarget.dataset.name,
+      songpic: e.currentTarget.dataset.songpic,
+      index: e.currentTarget.dataset.no,
+      id: e.currentTarget.dataset.id,
+      pid: e.currentTarget.dataset.pid,
+      duration: e.currentTarget.dataset.duration,
+      url: e.currentTarget.dataset.url
+    }
+    wx.setStorage({
+      key: "songInfo",
+      data: songInfo
+    })
+
     wx.navigateTo({
       url: `../playInfo/playInfo?sameFlag=${sameFlag}`
     })
