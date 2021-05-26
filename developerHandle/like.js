1800b642501f98e62c330aa3786771e93a94cbb2
zhuleishan <473668281@qq.com>
2021-05-26 11:59:32 +0800

    整理我的收藏代码

--- developerHandle/like.js
+++ developerHandle/like.js
@@ -19,217 +19,212 @@
  * 4、likePic: ['/images/info_like.png', '/images/info_like_no.png'],
  * 收藏和取消收藏图片
  */
-const app = getApp()
-import { albumFavorite, mediaFavorite, albumFavoriteCancel,albumFavoriteAdd,mediaFavoriteCancel,mediaFavoriteAdd, mediaUrlList } from '../utils/httpOpt/api'
+const app = getApp();
+import {
+  albumFavorite,
+  mediaFavorite,
+  albumFavoriteCancel,
+  albumFavoriteAdd,
+  mediaFavoriteCancel,
+  mediaFavoriteAdd,
+  mediaUrlList,
+} from "../utils/httpOpt/api";
 
 module.exports = {
   data: {
     info: [],
     showModal: false,
     req: false,
-    likePic: ['/images/info_like.png', '/images/info_like_no.png'],
+    likePic: ["/images/info_like.png", "/images/info_like_no.png"],
     labels: [
-      {value: 'album', name: '专辑'},
-      {value: 'media', name: '故事'}
+      { value: "album", name: "专辑" },
+      { value: "media", name: "故事" },
     ],
-    scrollLeft: 0
+    scrollLeft: 0,
   },
   onShow() {
-    if(!app.userInfo.token){
+    if (!app.userInfo.token) {
       wx.switchTab({
-        url: '/pages/personalCenter/personalCenter'
-      })
+        url: "/pages/personalCenter/personalCenter",
+      });
     }
   },
   onLoad(options) {
-    this._getList(this.data.labels[0].value)
+    this._getList(this.data.labels[0].value);
   },
-  onReady() {
-
-  },
+  onReady() {},
   // 跳转到播放详情界面
-  linkAbumInfo (e) {
-    let id = e.currentTarget.dataset.id
-    const src = e.currentTarget.dataset.src
-    const title = e.currentTarget.dataset.title
-    wx.setStorageSync('img', src)
-    const routeType = e.currentTarget.dataset.contentype
-
-    console.log(app.globalData.latelyListenId, routeType)
-    let url
-    if (routeType === 'album') {
-      url = `../abumInfo/abumInfo?id=${id}&title=${title}&routeType=${routeType}`
+  linkAbumInfo(e) {
+    let id = e.currentTarget.dataset.id;
+    const src = e.currentTarget.dataset.src;
+    const title = e.currentTarget.dataset.title;
+    wx.setStorageSync("img", src);
+    const routeType = e.currentTarget.dataset.contentype;
+    let url;
+    if (routeType === "album") {
+      url = `../abumInfo/abumInfo?id=${id}&title=${title}&routeType=${routeType}`;
       wx.navigateTo({
-        url: url
-      })
-    } else if (routeType === 'media') {
+        url: url,
+      });
+    } else if (routeType === "media") {
       let opt = {
         mediaId: id,
-        contentType: 'story'
-      }
-      mediaUrlList(opt).then(res2 => {
-        let canplay = res2.mediaPlayVoList
+        contentType: "story",
+      };
+      mediaUrlList(opt).then((res2) => {
+        let canplay = res2.mediaPlayVoList;
         canplay.map((item, index) => {
-          item.title = item.mediaName
-          item.id = item.mediaId
-          item.dt = item.timeText
-          item.coverImgUrl = item.coverUrl
-          item.dataUrl = item.mediaUrl
-        })
-        
-        wx.setStorageSync('canplay',canplay)
-        url = `../playInfo/playInfo?id=${id}`
+          item.title = item.mediaName;
+          item.id = item.mediaId;
+          item.dt = item.timeText;
+          item.coverImgUrl = item.coverUrl;
+          item.dataUrl = item.mediaUrl;
+        });
+
+        wx.setStorageSync("canplay", canplay);
+        url = `../playInfo/playInfo?id=${id}`;
         wx.navigateTo({
-          url: url
-        })
-      })
+          url: url,
+        });
+      });
     }
-    
-    
   },
   selectTap(e) {
-    const index = e.currentTarget.dataset.index
+    const index = e.currentTarget.dataset.index;
     this.setData({
       currentTap: index,
       req: false,
-      scrollLeft: 0
-    })
+      scrollLeft: 0,
+    });
     wx.showLoading({
-      title: '加载中',
-    })
-    this._getList(this.data.labels[index].value)
+      title: "加载中",
+    });
+    this._getList(this.data.labels[index].value);
   },
   _getList(value) {
-    console.log(value)
-    if (value == 'album'){
-      this.getAlbum()
-    } else if (value == 'media'){
-      this.getMedia()
+    if (value == "album") {
+      this.getAlbum();
+    } else if (value == "media") {
+      this.getMedia();
     }
   },
   getAlbum() {
     let params = {
       pageNum: 1,
-      pageSize: 20
-    }
-    albumFavorite(params).then(res => {
-      let layoutData = []
-      res.albumList.forEach(item => {
+      pageSize: 20,
+    };
+    albumFavorite(params)
+      .then((res) => {
+        let layoutData = [];
+        res.albumList.forEach((item) => {
           layoutData.push({
             id: item.albumId,
             title: item.albumName,
-            src: item.coverUrl, 
-            contentType: 'album',
-            // isVip: true
-            isVip: item.feeType == '01' && (item.product || item.product && [2, 3].indexOf(item.product.vipLabelType) < 0)
-          })
-      })
-      this.setData({
-        info: layoutData,
-        // info: [{id: 'qd223',title: '哈哈',src: "https://cdn.kaishuhezi.com/kstory/ablum/image/389e9f12-0c12-4df3-a06e-62a83fd923ab_info_w=450&h=450.jpg",contentType: 'album',isVip:true}],
-        req: true
-      })
-      if(layoutData.length === 0) {
+            src: item.coverUrl,
+            contentType: "album",
+            isVip:
+              item.feeType == "01" &&
+              (item.product ||
+                (item.product &&
+                  [2, 3].indexOf(item.product.vipLabelType) < 0)),
+          });
+        });
         this.setData({
-          showModal: true
-        })
-      }
-      wx.hideLoading()
-    }).catch(err => {
-      wx.hideLoading()
-      console.log(JSON.stringify(err)+'73行')
-    })
+          info: layoutData,
+          req: true,
+        });
+        if (layoutData.length === 0) {
+          this.setData({
+            showModal: true,
+          });
+        }
+        wx.hideLoading();
+      })
+      .catch((err) => {
+        wx.hideLoading();
+      });
   },
   getMedia() {
     let params = {
       pageNum: 1,
-      pageSize: 20
-    }
-    mediaFavorite(params).then(res => {
-      let layoutData = []
-      res.list.forEach(item => {
-          console.log(`${item}89行`)
-          // console.log(item.feeType == '01' && (item.product || item.product && [2, 3].indexOf(item.product.vipLabelType) < 0)+'57行')
+      pageSize: 20,
+    };
+    mediaFavorite(params)
+      .then((res) => {
+        let layoutData = [];
+        res.list.forEach((item) => {
           layoutData.push({
             id: item.mediaId,
             title: item.mediaName,
-            src: item.coverUrl, 
-            contentType: 'media'
-            // isVip: true
-          })
-
-      })
-      console.log(`${layoutData}67行`)
-      this.setData({
-        info: layoutData,
-        // info: [{id: 'qd223',title: '哈哈',src: "https://cdn.kaishuhezi.com/kstory/ablum/image/389e9f12-0c12-4df3-a06e-62a83fd923ab_info_w=450&h=450.jpg",contentType: 'album',isVip:true}],
-        req: true
-      })
-      if(layoutData.length === 0) {
+            src: item.coverUrl,
+            contentType: "media",
+          });
+        });
         this.setData({
-          showModal: true
-        })
-      }
-      wx.hideLoading()
-    }).catch(err => {
-      wx.hideLoading()
-      console.log(JSON.stringify(err)+'73行')
-    })
+          info: layoutData,
+          req: true,
+        });
+        if (layoutData.length === 0) {
+          this.setData({
+            showModal: true,
+          });
+        }
+        wx.hideLoading();
+      })
+      .catch((err) => {
+        wx.hideLoading();
+      });
   },
-  like (e) {
-    if(e.detail.contentType === 'album') {
-      this.likeAbum(e.detail.flag, e.detail.typeid)
-    } else if(e.detail.contentType === 'media') {
-      this.likeMedia(e.detail.flag, e.detail.typeid)
+  like(e) {
+    if (e.detail.contentType === "album") {
+      this.likeAbum(e.detail.flag, e.detail.typeid);
+    } else if (e.detail.contentType === "media") {
+      this.likeMedia(e.detail.flag, e.detail.typeid);
     }
   },
   likeAbum(flag, id) {
-    let { info } = this.data
+    let { info } = this.data;
     if (flag) {
-      console.log('albumFavoriteAdd')
-      albumFavoriteCancel({albumId: id}).then(res => {
-        wx.showToast({ icon: 'none', title: '取消收藏成功' })
+      albumFavoriteCancel({ albumId: id }).then((res) => {
+        wx.showToast({ icon: "none", title: "取消收藏成功" });
         this.setData({
           existed: false,
-          info:info.filter(item => item.id != id) || []
-        })
-      })
+          info: info.filter((item) => item.id != id) || [],
+        });
+      });
     } else {
-      albumFavoriteAdd({albumId: id}).then(res => {
-        wx.showToast({ icon: 'none', title: '收藏成功' })
+      albumFavoriteAdd({ albumId: id }).then((res) => {
+        wx.showToast({ icon: "none", title: "收藏成功" });
         this.setData({
-          existed: true
-        })
-      })
+          existed: true,
+        });
+      });
     }
-
-    
   },
-  likeMedia (flag, id) {
-    let { info } = this.data
+  likeMedia(flag, id) {
+    let { info } = this.data;
     if (flag) {
-      mediaFavoriteCancel({mediaId: id}).then(res => {
-        wx.showToast({ icon: 'none', title: '取消收藏成功' })
+      mediaFavoriteCancel({ mediaId: id }).then((res) => {
+        wx.showToast({ icon: "none", title: "取消收藏成功" });
         this.setData({
           existed: false,
-          info:info.filter(item => item.id != id) || []
-        })
-        let minibar = this.selectComponent('#miniPlayer')
-        minibar.setOnShow()
-      })
+          info: info.filter((item) => item.id != id) || [],
+        });
+        let minibar = this.selectComponent("#miniPlayer");
+        minibar.setOnShow();
+      });
     } else {
-      console.log('mediaFavoriteAdd')
-      mediaFavoriteAdd({mediaId: id}).then(res => {
-        wx.showToast({ icon: 'none', title: '收藏成功' })
+      mediaFavoriteAdd({ mediaId: id }).then((res) => {
+        wx.showToast({ icon: "none", title: "收藏成功" });
         this.setData({
-          existed: true
-        })
-        let minibar = this.selectComponent('#miniPlayer')
-        minibar.setOnShow()
-      })
+          existed: true,
+        });
+        let minibar = this.selectComponent("#miniPlayer");
+        minibar.setOnShow();
+      });
     }
   },
   close() {
-    this.setData({showModal: false})
-  }
-}
+    this.setData({ showModal: false });
+  },
+};
